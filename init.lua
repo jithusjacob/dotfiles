11a4d775328cfd35a22ed81d5beb5877aa3e025c
jithusjacob <jithusjacob@gmail.com>
2023-01-17 10:08:52 +0530

    checkpoint

--- init.lua
+++ init.lua
@@ -127,6 +127,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Project explorer 
+vim.keymap.set('n', '<leader>pf', ":Ex <cr>", { desc = '[P]roject [F]older' })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
